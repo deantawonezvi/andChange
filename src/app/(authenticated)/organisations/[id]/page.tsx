9bcd41c2bbe481624f645183bb5f6ca8984f3680
Roonie M <roonierue@gmail.com>
2025-02-03 12:54:12 +0200

    Add organisations page

---
+++ src/app/(authenticated)/organisations/[id]/page.tsx
@@ -0,0 +1,27 @@
+'use client'
+import React from 'react';
+import {Box} from '@mui/material';
+import {useQuery} from '@tanstack/react-query';
+import {OrganizationService} from "@/app/lib/api/services/organisationService";
+import OrganizationsTable from "@/app/lib/components/tables/organisationsTable";
+
+const OrganizationsPage = () => {
+    const organizationService = OrganizationService.getInstance();
+
+    const { data, isLoading, error } = useQuery({
+        queryKey: ['organizations'],
+        queryFn: () => organizationService.getAllOrganizations()
+    });
+
+    return (
+        <Box>
+            <OrganizationsTable
+                data={data || []}
+                isLoading={isLoading}
+                error={error instanceof Error ? error.message : undefined}
+            />
+        </Box>
+    );
+};
+
+export default OrganizationsPage;
